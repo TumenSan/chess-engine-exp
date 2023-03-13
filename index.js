fdaf20026b10bb2598a81009d7f75a0fe560bd77
TumenSan <sandanov.tumen.sib@mail.ru>
2023-03-14 01:45:48 +0700

    color and mini up

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-/*
-const stockfish = require('stockfish')
-const engine = await stockfish();
-
-engine.onmessage = function (message) {
-    console.log("received: " + message);
-}
-
-engine.postMessage('d');
-*/
-
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 6000
 const stockfish = require("stockfish");
 const engine = stockfish();
-const fenregex = "/^([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw]\s(-|K?Q?k?q?)\s(-|[a-h][36])\s(0|[1-9][0-9]*)\s([1-9][0-9]*)/"
+//const fenregex = "/^([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw]\s(-|K?Q?k?q?)\s(-|[a-h][36])\s(0|[1-9][0-9]*)\s([1-9][0-9]*)/"
 //const regex = "/info.*score\s(cp\s-?\d+)/g";
 
 engine.onmessage = function(msg) {
@@ -26,7 +15,15 @@ engine.onmessage = function(msg) {
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+app.get('/', (request, response) => {
+  response.send('post body any fen in /estimation or /bestmove');
+});
+
 app.post('/', (request, response) => {
+  response.send('post body any fen in /estimation or /bestmove');
+});
+
+app.post('/estimation', (request, response) => {
 
   //if (!request.body.fen.match(fenregex)) {
   if (!request.body.fen) {
@@ -45,16 +42,54 @@ app.post('/', (request, response) => {
     const regex = /^info depth 20 .* nodes \d+ .*$/;
     if (typeof(msg == "string") && (msg.match(regex) && !msg.includes("lowerbound") 
       && !msg.includes("upperbound"))) {
-        //response.send(msg);
-        response.send(msg);
+        let arr = fen.split(" ");
+        let color = "white move";
+        if(arr[1] == "w")
+          color = "white move";
+        else color = "black move";
+        response.send(`${color} ${msg}`);
     }
   }
 
+  let fen = request.body.fen;
+// run chess engine
+  engine.postMessage("ucinewgame");
+  engine.postMessage("position fen " + fen);
+  engine.postMessage("go depth 20");
+  //engine.postMessage("eval");//
+});
+
+app.post('/bestmove', (request, response) => {
+
+  //if (!request.body.fen.match(fenregex)) {
+  if (!request.body.fen) {
+    response.send("Invalid fen string");
+    return;
+  }
+  
+// if chess engine replies
+  engine.onmessage = function(msg) {
+    console.log(msg);
+    // in case the response has already been sent?
+    if (response.headersSent) {
+        return;
+    }
+    // only send response when it is a recommendation
+    if (typeof(msg == "string") && msg.match("bestmove")) {
+      let arr = fen.split(" ");
+      let color = "white move";
+      if(arr[1] == "w")
+        color = "white move";
+      else color = "black move";
+      response.send(`${color} ${msg}`);
+    }
+  }
+
+  let fen = request.body.fen;
 // run chess engine
   engine.postMessage("ucinewgame");
   engine.postMessage("position fen " + request.body.fen);
   engine.postMessage("go depth 20");
-  //engine.postMessage("eval");//
 });
 
 app.listen(port, (err) => {

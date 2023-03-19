9f6ef5b7b18eec3f403b9c488caee4ce6fd70883
TumenSan <sandanov.tumen.sib@mail.ru>
2023-03-20 00:39:29 +0700

    add rabbitmq

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ const stockfish = require("stockfish");
 const engine = stockfish();
 const fenMiddleware = require("./fenMiddleware");
 
+app.use(express.json());
+
+//брокер rabbitmq
+const amqp = require("amqplib");
+var channel, connection;
+
+connectQueue() // call connectQueue function
+async function connectQueue() {
+    try {
+
+        connection = await amqp.connect("amqp://localhost:5672");
+        channel = await connection.createChannel()
+        
+        // connect to 'test-queue', create one if doesnot exist already
+        await channel.assertQueue("test-queue")
+
+        channel.consume("test-queue", data => {
+          // console.log(data)
+          console.log("Data received : ", `${Buffer.from(data.content)}` );
+          channel.ack(data)
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+//движок
 engine.onmessage = function(msg) {
   console.log(msg);
 };
